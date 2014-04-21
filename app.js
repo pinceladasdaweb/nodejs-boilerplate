e5eacf94ea18b3d347b82f5152421721e93de962
Pedro Rogério <pinceladasdaweb@hotmail.com>
2014-04-21 18:15:36 -0300

    Created helper for date formatting via handlebars.js

--- app.js
+++ app.js
@@ -1,10 +1,20 @@
 var express = require('express'),
 	hbs = require('hbs'),
+	moment = require('moment'),
 	router = require(__dirname + '/routes').router,
 	app = express(),
 	port = process.env.PORT || 3002;
 
 hbs.registerPartials(__dirname + '/views/partials');
+
+hbs.registerHelper('dateFormat', function(context, block) {
+	if (moment) {
+		var f = block.hash.format || "MMM DD, YYYY hh:mm:ss A";
+			return moment(context).format(f);
+		} else {
+			return context;
+	};
+});
 
 app.set('view engine', 'html');
 app.set('views', __dirname + '/views/pages');
