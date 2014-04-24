b236995d90134f3fab808566b013a5be6f874908
Pedro Rogerio <pedro.lemes@locaweb.com.br>
2014-04-24 19:24:50 -0300

    Removed unnecessary condition

--- app.js
+++ app.js
@@ -8,12 +8,8 @@ var express = require('express'),
 hbs.registerPartials(__dirname + '/views/partials');
 
 hbs.registerHelper('dateFormat', function(context, block) {
-	if (moment) {
-		var f = block.hash.format || "MMM DD, YYYY hh:mm:ss A";
-			return moment(context).format(f);
-		} else {
-			return context;
-	};
+	var f = block.hash.format || "MMM DD, YYYY hh:mm:ss A";
+	return moment(context).format(f);
 });
 
 app.set('view engine', 'html');
