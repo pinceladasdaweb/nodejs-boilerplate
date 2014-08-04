54f4b2969a8e016fed1deab0bd444e31e6c88391
Renan Pimentel <cad_rpimentel@uolinc.com>
2014-08-04 17:06:11 -0300

    remove test

--- middleware/error.js
+++ middleware/error.js
@@ -1,6 +1,6 @@
 exports.notFound = function(req, res, next) {
   res.status(404);
-  res.render('404', {error: 'teste'})
+  res.render('404')
 };
 
 exports.serverError = function(error, req, res, next) {
